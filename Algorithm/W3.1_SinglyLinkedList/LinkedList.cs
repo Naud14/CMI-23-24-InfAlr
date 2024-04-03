f5dd773dd04e12d792b7c21976af3b30a2e213cb
Naud14 <n.drost14@gmail.com>
2024-04-03 18:40:03 +0200

    LinkedLists

---
+++ Algorithm/W3.1_SinglyLinkedList/LinkedList.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+
+namespace Solution;
+
+public class SinglyLinkedList<T> : ILinkedList<T> where T : IComparable<T>
+{
+    public SingleNode<T>? Head;
+    private int count;
+
+    public SinglyLinkedList()
+    {
+        Head = null;
+        count = 0;
+    }
+
+    public int Count => count;
+
+    public void AddFirst(T value)
+    {
+        Head = new SingleNode<T>(value, null);
+        count ++;
+    }
+
+    public void AddLast(T value)
+    {
+        if (Head == null)
+        {
+            AddFirst(value);
+            return;
+        }
+
+        SingleNode<T> curr = Head;
+        while (curr.Next != null)
+        {
+            curr = curr.Next;
+        }
+
+        curr.Next = new SingleNode<T>(value, null);
+        count++;
+    }
+
+    public bool Remove(T value)
+    {
+        if(Head == null) return false;
+        if(Head.Value.CompareTo(value) == 0)
+        {
+            Head = Head.Next; 
+            count--;
+            return true;
+        }
+        
+        SingleNode<T> curr = Head;
+        while(curr.Next != null)
+        {
+            if(curr.Next.Value.CompareTo(value) == 0)
+            {
+                curr.Next = curr.Next.Next;
+                count--;
+                return true;
+            }
+            curr = curr.Next;
+        }
+        return false;
+        
+    }
+
+    public SingleNode<T>? Search(T value)
+    {
+        if(Head == null) return default;
+        SingleNode<T> curr = Head;
+        while(curr != null)
+        {
+            if(curr.Value.CompareTo(value) == 0) return curr;
+            curr = curr.Next;
+        }
+        return null;
+    }
+
+    public bool Contains(T value) => Search(value) != null;
+
+    public void AddSorted(T value)
+    {
+        if(Head == null || Head.Value.CompareTo(value) > 0)
+        {
+            Head = new SingleNode<T>(value, Head);
+            count++;
+            return;
+        }
+        SingleNode<T>? current = Head;
+        while (current.Next != null && current.Next.Value.CompareTo(value) < 0)
+        {
+            current = current.Next;
+        }
+        current.Next = new SingleNode<T>(value, current.Next);
+        count++;
+    }
+
+    public void Clear()
+    {
+        Head = null;
+        count = 0;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        SingleNode<T>? current = Head;
+        while (current != null)
+        {
+            yield return current.Value;
+            current = current.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+}
--- Algorithm/W3.1_SinglyLinkedList/Program.cs
+++ Algorithm/W3.1_SinglyLinkedList/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Solution; 
+SinglyLinkedList<int> intList = new SinglyLinkedList<int>();
+
+if(intList != null) {
+    intList.AddSorted(20);
+    intList.AddSorted(40);
+    intList.AddSorted(30);
+    intList.AddSorted(10);
+    foreach(var el in intList) {
+        System.Console.WriteLine(el);
+    }
+    int[] actual = new int[] { intList.Head.Value, intList.Head.Next.Value, intList.Head.Next.Next.Value, intList.Head.Next.Next.Next.Value };
+    int[] expected = new int[] { 10, 20, 30, 40 };
+}
